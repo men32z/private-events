4460678e1dc4affabbfb55b5fccf95e7f7927462
Luis Preza <preza.luis@gmail.com>
2019-11-29 18:05:03 -0600

    header added, controllers for sessions and static pages, testing for sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  include SessionsHelper
+  
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= (session[:user_id] ? User.find(session[:user_id]): nil)
+  end
+
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def sign_out
+    session[:user_id] = nil
+    @current_user = nil
+    redirect_to home_path
+  end
+end
